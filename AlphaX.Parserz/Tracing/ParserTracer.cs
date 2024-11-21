0001b6d13dd7ca866fe26bf654522b90cbed3038
Kartik.Sagar <Kartik.Sagar@IN-LAP-KARTIK>
2024-11-21 14:57:59 +0530

    #2 Return raw trace from parser tracer

--- AlphaX.Parserz/Tracing/ParserTracer.cs
+++ AlphaX.Parserz/Tracing/ParserTracer.cs
@@ -1,53 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AlphaX.Parserz.Tracing
 {
     public static class ParserTracer
     {
-        internal static List<string> _trace;
-
-        internal const string BEGIN_TRACE_FORMAT = "{0} > Parsing '{1}' at index '{2}'";
-        internal const string RESULT_TRACE_FORMAT = "{0} > Parsed & left with '{1}'";
-        internal const string ERROR_TRACE_FORMAT = "{0} > Parse Failed. {1}";
+        internal static ConcurrentStack<Trace> _trace;
 
         public static bool Enabled {  get; set; }
 
         static ParserTracer()
         {
-            _trace = new List<string>();
+            _trace = new ConcurrentStack<Trace>();
             Enabled = false;
         }
 
         public static void Trace(IParser parser, IParserState state, bool isResult)
         {
-            if (state.IsError)
-            {
-                _trace.Add(string.Format(ERROR_TRACE_FORMAT,
-                    parser.GetType().Name,
-                    state.Error
-                ));
-            }
-            else if(isResult)
-            {
-                _trace.Add(string.Format(RESULT_TRACE_FORMAT,
-                   parser,
-                   state.Input
-                ));
-            }
-            else
-            {
-                _trace.Add(string.Format(BEGIN_TRACE_FORMAT,
-                   parser,
-                   state.ActualInput,
-                   state.Index
-                ));
-            }    
+            _trace.Push(new Trace(parser, state, isResult));
         }
 
-        public static IEnumerable<string> GetTrace()
+        public static IEnumerable<Trace> GetTraces()
         {
-            return _trace.AsEnumerable();
+            return _trace.AsEnumerable().Reverse();
         }
 
         public static void Reset()
@@ -55,4 +32,24 @@ namespace AlphaX.Parserz.Tracing
             _trace.Clear();
         }
     }
+
+    public class Trace : IDisposable
+    {
+        public string Parser { get; }
+        public bool IsInput { get; }
+        public IParserState State { get; private set; }
+
+        public Trace(IParser parser, IParserState state, bool isResult)
+        {
+            Parser = parser.GetType().Name;
+            State = state.Clone();
+            IsInput = !isResult;
+        }
+
+        public void Dispose()
+        {
+            State = null;
+        }
+    }
+
 }
